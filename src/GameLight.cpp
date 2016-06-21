534eb60e01c4707894593e402c75877f94f9d21d
mafoltz <marcelo.andrighetto@hotmail.com>
2016-06-21 15:30:46 -0300

    Add files via upload
    Luzinhas do sol brilhante :D

---
+++ src/GameLight.cpp
@@ -0,0 +1,23 @@
+#include "GameLight.h"
+
+GameLight::GameLight()
+{
+    glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	GLfloat light_ambient[] = {skyColor.r, skyColor.g, skyColor.b, 1.0};
+	GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat light_position[] = {0.0, 0.0, 0.0, 1.0};
+	GLfloat light_direction[] = {0.0, 0.0, -1.0, 1.0};
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_direction);
+}
+
+GameLight::~GameLight()
+{
+    glDisable(GL_LIGHT0);
+    glDisable(GL_LIGHTING);
+}
---
+++ src/GameLight.h
@@ -0,0 +1,17 @@
+#ifndef GAMELIGHT_H
+#define GAMELIGHT_H
+
+#include <gl/gl.h>
+#include <gl/glut.h>
+
+class GameLight
+{
+    public:
+        GameLight();
+        virtual ~GameLight();
+
+    private:
+        const A_RGB skyColor = A_RGB(0, 185, 255);
+};
+
+#endif // GAMELIGHT_H
