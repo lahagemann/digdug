68abe3996a7631026f176541eff107f5894c927f
lahagemann <lahagemann@inf.ufrgs.br>
2016-06-23 12:33:29 -0300

    exclusão da game, info colocada na main

--- CodeBlocks/DiglettDug/main.cpp
+++ CodeBlocks/DiglettDug/main.cpp
@@ -1,17 +1,3 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -22,158 +8,418 @@
 #include <gl/glut.h>
 #include <stdlib.h>
 
-static int slices = 16;
-static int stacks = 16;
+#include "Cam.h"
+#include "GameLight.h"
+#include "GameMap.h"
+#include "GameSettings.h"
 
-/* GLUT callback Handlers */
+/* function declaration */
+void initTexture(void);
+void mainRender();
+void mainInit();
+void onKeyDown(unsigned char key, int x, int y);
+void onKeyUp(unsigned char key, int x, int y);
+void onMouseButton(int button, int state, int x, int y);
+void onMouseMove(int x, int y);
+void onMousePassiveMove(int x, int y);
+void onWindowReshape(int x, int y);
+void renderScene();
+void renderSea();
+void setViewport(GLint left, GLint right, GLint bottom, GLint top);
+void setWindow();
+void updateState();
 
-static void resize(int width, int height)
+/* global var declaration */
+const char* GAME_NAME = "Dig(lett) Dug(trio)";
+int mainWindowId = 0;
+int MouseXPosition = 0;
+int MouseYPosition = 0;
+float planeSize = 31.0f;
+GLuint texture;         /* Texture object */
+GLenum type;            /* Texture type */
+int windowHeight = 480;
+int windowWidth = 600;
+int windowXPos = 100;
+int windowYPos = 150;
+
+/* game objects declaration */
+Cam cam;
+GameLight light;
+GameMap game_map;
+GameSettings settings;
+
+/* key pressing verification */
+bool backPressed;
+bool changeCamera;
+bool makeCrackPressed;
+bool pausePressed;
+bool pushPressed;
+bool rotateLeftPressed;
+bool rotateRightPressed;
+bool walkPressed;
+
+void initTexture()
 {
-    const float ar = (float) width / (float) height;
+    BITMAPINFO	*info;           /* Bitmap information */
+    GLubyte	    *rgba;           /* RGBA pixel buffer */
+    GLubyte	    *rgbaptr;        /* Pointer into RGBA buffer */
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+    // Load a texture object (256x256 true color)
+    bits = LoadDIBitmap("tiledbronze.bmp", &info);
+    if (bits == (GLubyte *)0)
+		return;
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
-}
+    // Figure out the type of texture
+    if (info->bmiHeader.biHeight == 1)
+      type = GL_TEXTURE_1D;
+    else
+      type = GL_TEXTURE_2D;
 
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+    // Create and bind a texture object
+    glGenTextures(1, &texture);
+	glBindTexture(type, texture);
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
+    // Create an RGBA image
+    rgba = (GLubyte *)malloc(info->bmiHeader.biWidth * info->bmiHeader.biHeight * 4);
 
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-
-
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
+    int i = info->bmiHeader.biWidth * info->bmiHeader.biHeight;
+    for( rgbaptr = rgba, ptr = bits;  i > 0; i--, rgbaptr += 4, ptr += 3)
     {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+            rgbaptr[0] = ptr[2];     // windows BMP = BGR
+            rgbaptr[1] = ptr[1];
+            rgbaptr[2] = ptr[0];
+            rgbaptr[3] = (ptr[0] + ptr[1] + ptr[2]) / 3;
     }
 
-    glutPostRedisplay();
+	// Set texture parameters
+	glTexParameteri(type, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(type, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(type, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(type, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+    glTexImage2D(type, 0, 4, info->bmiHeader.biWidth, info->bmiHeader.biHeight,
+                  0, GL_RGBA, GL_UNSIGNED_BYTE, rgba );
 }
 
-static void idle(void)
+void mainInit()
+{
+    glClearColor(1.0,1.0,1.0,0.0);
+	glColor3f(0.0f,0.0f,0.0f);
+	setWindow();
+	setViewport(0, windowWidth, 0, windowHeight);
+
+	// habilita o z-buffer
+	glEnable(GL_DEPTH_TEST);
+    initTexture();
+}
+
+void mainRender()
+{
+    updateState();
+	renderScene();
+	glFlush();
+	glutPostRedisplay();
+	Sleep(30);
+}
+
+void onKeyDown(unsigned char key, int x, int y)
+{
+    switch(tolower(key))
+    {
+        case settings.walk_forward:
+            walkPressed = true;
+            break;
+        case settings.walk_back:
+            backPressed = true;
+            break;
+        case settings.rotate_right:
+            rotateRightPressed = true;
+            break;
+        case settings.rotate_left:
+            rotateLeftPressed = true;
+            break;
+        case settings.change_camera:
+            changeCamera = true;
+            break;
+        case settings.make_crack:
+            makeCrackPressed = true;
+            break;
+        case settings.push_enemy:
+            pushPressed = true;
+            break;
+        case settings.pause:
+            pausePressed = true;
+            break;
+        case settings.quit:
+            exit(0);
+        default:
+            break;
+    }
+}
+
+void onKeyUp(unsigned char key, int x, int y)
+{
+    switch(tolower(key))
+    {
+        case settings.walk_forward:
+            walkPressed = false;
+            break;
+        case settings.walk_back:
+            backPressed = false;
+            break;
+        case settings.rotate_right:
+            rotateRightPressed = false;
+            break;
+        case settings.rotate_left:
+            rotateLeftPressed = false;
+            break;
+        case settings.change_camera:
+            changeCamera = false;
+            break;
+        case settings.make_crack:
+            makeCrackPressed = false;
+            break;
+        case settings.push_enemy:
+            pushPressed = false;
+            break;
+        case settings.pause:
+            pausePressed = false;
+            break;
+        case settings.quit:
+            exit(0);
+        default:
+            break;
+    }
+}
+
+void onMouseButton(int button, int state, int x, int y)
 {
     glutPostRedisplay();
 }
 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+void onMouseMove(int x, int y)
+{
+    glutPostRedisplay();
+}
 
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
+void onMousePassiveMove(int x, int y)
+{
+    game_map.player.setXRotation(y - MouseYPosition);
+    game_map.player.setYRotation(x - MouseXPosition);
+
+	if(game_map.player.getXRotation() < -128.0)
+		game_map.player.getXRotation() = -128.0;
+
+	if(game_map.player.getXRotation() > -45.0)
+		game_map.player.getXRotation() = -45.0;
+
+	MouseXPosition = x;
+	MouseYPosition = y;
+}
+
+void onWindowReshape(int x, int y)
+{
+    windowWidth = x;
+	windowHeight = y;
+	setWindow();
+	setViewport(0, windowWidth, 0, windowHeight);
+}
+
+void renderScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // limpar o depth buffer
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	updateCam();
+
+    float x, z;
+	// loading player...
+
+	game_map.player.getPosition().convert_to_xz(&x, &z);
+
+	glPushMatrix();
+        glTranslatef(x,0.0f,z);
+        glmDraw(game_map.player.getModel(), GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
+    glPopMatrix();
+
+    // loading enemies...
+    for(int i = 0; i < game_map.characters.size(); i++)
+    {
+        Character c = game_map.characters.at(i);
+        c.getPosition().convert_to_xz(&x, &z);
+
+        glPushMatrix();
+            glTranslatef(x,0.0f,z);
+            glmDraw(c.getModel(), GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
+        glPopMatrix();
+    }
+
+    for(int i = 0; i < game_map.stage_map.size())
+    {
+        for(int j = 0; j < game_map.stage_map.at(i).size())
+        {
+            A_RGB rgb = game_map.stage_map.at(i).at(j);
+            if(rgb.isBlack())
+            {
+                Hole hole = Hole(i,j);
+                hole.getPosition().convert_to_xz(&x, &z);
+
+                glPushMatrix();
+                    glTranslatef(x,-0.5f,z);
+                    glmDraw(hole.getModel(), GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
+                glPopMatrix();
+            }
+            else if(rgb.isGreen())
+            {
+                Floor floor = Floor(i,j);
+                floor.getPosition().convert_to_xz(&x, &z);
+
+                glPushMatrix();
+                    glTranslatef(x,-0.5f,z);
+                    glmDraw(floor.getModel(), GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
+                glPopMatrix();
+            }
+            else if(rgb.isRed())
+            {
+                Crack crack = Crack(i,j);
+                crack.getPosition().convert_to_xz(&x, &z);
+
+                glPushMatrix();
+                    glTranslatef(x,-0.5f,z);
+                    glmDraw(crack.getModel(), GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
+                glPopMatrix();
+            }
+        }
+    }
+
+    glBindTexture(type, texture);
+
+	renderSea();
+}
+
+void renderSea()
+{
+    // set things up to render the floor with the texture
+	glShadeModel(GL_SMOOTH);
+	glEnable(type);
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	glPushMatrix();
+
+    glTranslatef(-(float)planeSize/2.0f, 0.0f, -(float)planeSize/2.0f);
+
+	float textureScaleX = 10.0;
+	float textureScaleY = 10.0;
+    glColor4f(1.0f,1.0f,1.0f,1.0f);
+    int xQuads = 40;
+    int zQuads = 40;
+    for (int i = 0; i < xQuads; i++) {
+        for (int j = 0; j < zQuads; j++) {
+            glBegin(GL_QUADS);
+                glTexCoord2f(1.0f, 0.0f);   // coords for the texture
+                glNormal3f(0.0f,1.0f,0.0f);
+                glVertex3f(i * (float)planeSize/xQuads, 0.0f, (j+1) * (float)planeSize/zQuads);
+
+                glTexCoord2f(0.0f, 0.0f);  // coords for the texture
+                glNormal3f(0.0f,1.0f,0.0f);
+                glVertex3f((i+1) * (float)planeSize/xQuads, 0.0f, (j+1) * (float)planeSize/zQuads);
+
+                glTexCoord2f(0.0f, 1.0f);  // coords for the texture
+                glNormal3f(0.0f,1.0f,0.0f);
+                glVertex3f((i+1) * (float)planeSize/xQuads, 0.0f, j * (float)planeSize/zQuads);
+
+                glTexCoord2f(1.0f, 1.0f);  // coords for the texture
+                glNormal3f(0.0f,1.0f,0.0f);
+                glVertex3f(i * (float)planeSize/xQuads, 0.0f, j * (float)planeSize/zQuads);
+
+            glEnd();
+        }
+    }
+
+	glDisable(type);
+	glPopMatrix();
+}
+
+void setViewport(GLint left, GLint right, GLint bottom, GLint top)
+{
+	glViewport(left, bottom, right - left, top - bottom);
+}
+
+void setWindow()
+{
+    glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,(GLfloat)windowWidth/(GLfloat)windowHeight,0.1f, 100.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void updateState()
+{
+    if(walkPressed)
+    {
+        if(!game_map.checkObstacleCollision())
+            game_map.player.walk(Diglett::Direction::forwards);
+    }
+
+
+    if(backPressed)
+    {
+        if(!game_map.checkObstacleCollision())
+            game_map.player.walk(Diglett::Direction::forwards);
+    }
+
+    if(rotateLeftPressed)
+        game_map.player.walk(Diglett::Direction::rotateLeft);
+
+    if(rotateRightPressed)
+        game_map.player.walk(Diglett::Direction::rotateRight);
+
+    if(makeCrackPressed)
+        game_map.makeCrack();
+
+    if(pushPressed)
+        game_map.push();
+
+    if(changeCamera)
+    {
+        cam.changeCam(game_map.player);
+        changeCamera = false;
+    }
+
+    game_map.moveEnemies();
+
+    if(game_map.isPlayerDead())
+            //death
+}
 
 /* Program entry point */
 
 int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutInitWindowPosition(windowXPos, windowYPos);
 
-    glutCreateWindow("GLUT Shapes");
+	// Store main window id so that glui can send it redisplay events
+	mainWindowId = glutCreateWindow(GAME_NAME);
+	glutDisplayFunc(mainRender);
+	glutReshapeFunc(onWindowReshape);
 
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
+	// Register mouse events handlers
+	glutMouseFunc(onMouseButton);
+	glutMotionFunc(onMouseMove);
+	glutPassiveMotionFunc(onMousePassiveMove);
 
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+	// Register keyboard events handlers
+	glutKeyboardFunc(onKeyDown);
+	glutKeyboardUpFunc(onKeyUp);
+	mainInit();
+	glutMainLoop();
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
